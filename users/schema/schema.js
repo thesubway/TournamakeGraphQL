4b51ac6ac0d35b9a794efd39700bd7944ef8b8b2
thesubway <thesubway@gmail.com>
2018-01-21 00:16:39 -0800

    Add missing resolve

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,5 +1,7 @@
 //defines what properties each object has,
 //and exactly how each object is related to each other
+
+//schema is probably 50% of all the graphql work
 const graphql = require('graphql');
 const {
   GraphQLObjectType,
@@ -11,7 +13,7 @@ const _ = require('lodash');
 
 //hardcode users:
 const users = [
-  {id: '23', firstName: 'Bill', age: 20},
+  {id: '23', firstName: 'Bill', age: 21},
   {id: '47', firstName: 'Samantha', age: 21}
 ]
 
@@ -34,7 +36,9 @@ const RootQuery = new GraphQLObjectType({
       //parentValue is notorious for not being used almost ever
       resolve(parentValue, args) {
           //very important function in GraphQL
-          //oh, we're looking for user with id 23
+          //"oh, we're looking for user with id 23"
+          //this function wants to return a user
+          return _.find(users, {id: args.id});
       }
     }
   }
